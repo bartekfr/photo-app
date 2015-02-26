4a1f84a6b3d08c16af757a597d069ad411990ca0
Bartek Fryzowicz <bartekfr@wp.pl>
2015-02-26 13:51:44 +0100

    New report: redirect after success

--- src/app/admin/admin.js
+++ src/app/admin/admin.js
@@ -1,14 +1,17 @@
 angular.module("admin", [])
 .controller("adminCtrl", ["$scope", function ($scope) {
 }])
-.controller("reportAddCtrl", ["$scope", "reports", function ($scope, reports) {
+.controller("reportAddCtrl", ["$scope", "reports", "$state", "messenger", function ($scope, reports, $state, messenger) {
 	$scope.report = {
 		data: [],
 		title: ""
 	};
 
 	$scope.save = function() {
-		reports.save($scope.report);
+		reports.save($scope.report).$promise.then(function() {
+			$state.go("home");
+			messenger.log("New report added");
+		});
 	};
 }])
 .controller("reportEditCtrl", ["$scope", "report", "$state", "messenger", function ($scope, report, $state, messenger) {
